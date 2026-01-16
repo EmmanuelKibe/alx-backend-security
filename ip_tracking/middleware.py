4f3096a1418c1f396574093c0c4c4c20d9d20643
EmmanuelKibe <emmanuelkibe148@gmail.com>
2026-01-16 23:48:59 +0300

    Use another Ip

--- ip_tracking/middleware.py
+++ ip_tracking/middleware.py
@@ -16,7 +16,7 @@ class IPTrackingMiddleware:
             ip = x_forwarded_for.split(',')[0]
         else:
             ip = request.META.get('REMOTE_ADDR') """
-        ip = "8.8.8.8"  # For testing purposes
+        ip = "103.230.15.119"  # For testing purposes
 
         # Create the log entry in the database
         if ip:
