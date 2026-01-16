de7b80733cebb9f33ebf4c5a7700e91d679069fc
EmmanuelKibe <emmanuelkibe148@gmail.com>
2026-01-16 23:00:29 +0300

    Update middleware to populate the RequestLog model fields

--- ip_tracking/middleware.py
+++ ip_tracking/middleware.py
@@ -1,8 +1,13 @@
 from .models import RequestLog, BlockedIP
+from django.http import HttpResponseForbidden
+import ipinfo
+from django.core.cache import cache
+from django.conf import settings
 
 class IPTrackingMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
+        self.handler = ipinfo.getHandler(getattr(settings, 'IPINFO_TOKEN', None))
 
     def __call__(self, request):
         # Extract IP address
@@ -18,9 +23,27 @@ class IPTrackingMiddleware:
             if BlockedIP.objects.filter(ip_address=ip).exists():
                 return HttpResponseForbidden("Access Denied: Your IP has been blacklisted.")
 
+            cache_key = f"geo_{ip}"
+            geo_data = cache.get(cache_key)
+
+            if not geo_data:
+                try:
+                    #Call API if not in cache
+                    details = self.handler.getDetails(ip)
+                    geo_data = {
+                        'country': details.country_name,
+                        'city': details.city
+                    }
+                    # Cache for 24 hours (86400 seconds)
+                    cache.set(cache_key, geo_data, 86400)
+                except Exception:
+                    geo_data = {'country': 'Unknown', 'city': 'Unknown'}
+
             RequestLog.objects.create(
                 ip_address=ip,
-                path=request.path
+                path=request.path,
+                country=geo_data.get('country'),
+                city=geo_data.get('city')
             )
 
         response = self.get_response(request)
