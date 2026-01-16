997e214837472ce6211f32aa7ca2b351f641a2cb
EmmanuelKibe <emmanuelkibe148@gmail.com>
2026-01-16 20:02:11 +0300

    Create a model to store blocked IPs

--- ip_tracking/models.py
+++ ip_tracking/models.py
@@ -8,3 +8,12 @@ class RequestLog(models.Model):
 
     def __str__(self):
         return f"{self.ip_address} accessed {self.path} at {self.timestamp}"
+
+#Create a model to store blocked IPs
+class BlockedIP(models.Model):
+    ip_address = models.GenericIPAddressField(unique=True)
+    reason = models.CharField(max_length=255, blank=True)
+    blocked_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Blocked: {self.ip_address}"
