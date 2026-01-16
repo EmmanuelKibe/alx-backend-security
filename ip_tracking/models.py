83d3bc8a7103ef7eadc5dcf205f2300faff1e724
EmmanuelKibe <emmanuelkibe148@gmail.com>
2026-01-16 23:29:00 +0300

    Extend RequestLog to contain city and country

--- ip_tracking/models.py
+++ ip_tracking/models.py
@@ -5,6 +5,8 @@ class RequestLog(models.Model):
     ip_address = models.GenericIPAddressField()
     path = models.CharField(max_length=255)
     timestamp = models.DateTimeField(auto_now_add=True)
+    country = models.CharField(max_length=100, null=True, blank=True)
+    city = models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self):
         return f"{self.ip_address} accessed {self.path} at {self.timestamp}"
@@ -14,8 +16,6 @@ class BlockedIP(models.Model):
     ip_address = models.GenericIPAddressField(unique=True)
     reason = models.CharField(max_length=255, blank=True)
     blocked_at = models.DateTimeField(auto_now_add=True)
-    country = models.CharField(max_length=100, null=True, blank=True)
-    city = models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self):
         return f"Blocked: {self.ip_address}"
