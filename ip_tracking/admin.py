28005247d320bafafe384a464656156093d2f46e
EmmanuelKibe <emmanuelkibe148@gmail.com>
2026-01-16 23:41:48 +0300

    Modify the display and filters of the models

--- ip_tracking/admin.py
+++ ip_tracking/admin.py
@@ -2,6 +2,16 @@ from django.contrib import admin
 from .models import RequestLog, BlockedIP
 
 # Register your models here.
-admin.site.register(RequestLog)
-admin.site.register(BlockedIP)
+from django.contrib import admin
+from .models import RequestLog, BlockedIP
+
+@admin.register(RequestLog)
+class RequestLogAdmin(admin.ModelAdmin):
+    list_display = ('ip_address', 'country', 'city', 'path', 'timestamp')
+    list_filter = ('country', 'timestamp')
+    search_fields = ('ip_address', 'city')
+
+@admin.register(BlockedIP)
+class BlockedIPAdmin(admin.ModelAdmin):
+    list_display = ('ip_address',)
 
