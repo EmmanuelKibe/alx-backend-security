f9c9525d246a2819cbd49b752d99e567c90a4efe
EmmanuelKibe <emmanuelkibe148@gmail.com>
2026-01-17 00:19:39 +0300

    Create a sensitive view to incorporate rate limiting

--- ip_tracking/views.py
+++ ip_tracking/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django_ratelimit.decorators import ratelimit
 
-# Create your views here.
+# This function determines the rate based on authentication status
+def get_rate(group, request):
+    if request.user.is_authenticated:
+        return '10/m'  # 10 requests per minute for logged-in users
+    return '5/m'       # 5 requests per minute for anonymous users
+
+@ratelimit(key='ip', rate=get_rate, method='ALL', block=True)
+def sensitive_login_view(request):
+    return HttpResponse("Welcome to the Login Page. Please enter your credentials.")
+
+
